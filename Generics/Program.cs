7152f3c4a1c04b2af0d1dc344647d4a3e685c12c
“sarthak <sarthakgangrade0@gmail.com>
2021-12-26 19:38:44 +0530

    add programm to return the maximum integer out of 3 integers

---
+++ Generics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the First number:");
+            int f = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int s = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number:");
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            SimpleClass simple = new SimpleClass(f, s, t);
+            simple.findMax();
+        }
+    }
+}
---
+++ Generics/SimpleClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class SimpleClass
+    {
+        public int first, second, third;
+        
+        public SimpleClass(int first, int second, int third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+
+        }
+
+        public void findMax()
+        {
+            //here checking the greater number among three using compareTo() method
+
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine("the {0} is greater", first);//if first is greater 
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine("the {0} is greater", second);//if second is greater
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine("the {0} is greater", third);//if third is greater
+            }
+            else
+            {
+                Console.WriteLine("numbers are equal");//else all number are equal
+            }
+            Console.ReadLine();
+        }
+    }
+}
