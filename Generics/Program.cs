0a6a49da1a966838e2f08a51ef39322af919a6e4
“sarthak <sarthakgangrade0@gmail.com>
2021-12-26 22:16:43 +0530

    add programm to return the maximum float number out of 3 float numbers

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -12,14 +12,23 @@ namespace Generics
 
         static void Main(string[] args)
         {
+            /* Console.WriteLine("Enter the First number:");
+             int f = Convert.ToInt32(Console.ReadLine());
+
+             Console.WriteLine("Enter the second number:");
+             int s = Convert.ToInt32(Console.ReadLine());
+
+             Console.WriteLine("Enter the third number:");
+             int t = Convert.ToInt32(Console.ReadLine());*/
+
             Console.WriteLine("Enter the First number:");
-            int f = Convert.ToInt32(Console.ReadLine());
+            float f = Convert.ToSingle(Console.ReadLine());
 
             Console.WriteLine("Enter the second number:");
-            int s = Convert.ToInt32(Console.ReadLine());
+            float s = Convert.ToSingle(Console.ReadLine());
 
             Console.WriteLine("Enter the third number:");
-            int t = Convert.ToInt32(Console.ReadLine());
+            float t = Convert.ToSingle(Console.ReadLine());
 
             SimpleClass simple = new SimpleClass(f, s, t);
             simple.findMax();
